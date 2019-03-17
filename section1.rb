8e4f434e3a088c127a14f3d636bd13b248aa802e
yuki <yuki@yuki.yuki>
2019-03-18 00:57:21 +0900

    ２日目

--- section1.rb
+++ section1.rb
@@ -11,10 +11,10 @@ people = %w{alice bob carol}
 person_0th = people[0]
 person_9th = people[9]
 
-puts person_0th&.capitalize
+#puts person_0th&.capitalize
 puts person_0th.capitalize
 
-puts person_9th&.capitalize
+#puts person_9th&.capitalize
 begin
     puts person_9th.capitalize
 rescue => e
@@ -24,8 +24,8 @@ end
 
 puts '************************************************************'
 def greet(name, *message)
-  message.each do |message|
-    puts "#{message},#{name}"
+  message.each do |msg|
+    puts "#{msg},#{name}"
   end
 end
 
@@ -53,6 +53,77 @@ block_sample do
   puts 'walk2'
 end
 
+def block_sample2
+  puts "stand up"
+
+  yield if block_given?
+
+  puts "sit down"
+end
+
+block_sample2 do
+  puts "walk"
+  puts "walk2"
+end
+
+puts '************************************************************'
+
+File.open 'test.txt' do |t|
+  puts t.read
+end
+
+f = File.open('test2.xxx', 'w')
+f.puts 'without block'
+f.close
+
+File.open 'test3.xxx', 'w' do |t|
+  t.puts 'with block'
+end
+
+puts '************************************************************'
+
+#関数戻り値設定
+def call
+  'called'
+end
+
+puts call()
+
+def greet2(resipient)
+  return false unless resipient
+
+  "Hi,#{resipient.capitalize}."
+end
+
+puts greet2('alice')
+puts greet2(nil)
+
+
+def greet3(name, message = 'Hi')
+  puts "#{message}, #{name}."
+end
+
+greet3("斎藤")
+greet3("斎藤", "こんにちは")
+greet3("斎藤", nil)
+
+def greet4(name, message = 'Hi', suffix = '.')
+  puts "#{message}, #{name + suffix}"
+end
+
+greet4('Ruby')
+greet4('Ruby', 'Hello')
+greet4('Ruby', 'Hello', ':')
+
+def greet5(name, *messages)
+  messages.each do |message|
+    puts "#{message}, #{name}."
+  end
+end
+
+greet5 "Ruby", "Hello", "こんにちは"
+
+puts '************************************************************'
 
 
 
